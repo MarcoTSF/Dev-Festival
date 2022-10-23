b26c40da1283052a7e015c5ac09b9c169eec6ac3
MarcoTSF <mtsf.salvador@gmail.com>
2022-10-23 06:06:49 -0300

    Dev Festival - v1.5
    Projeto base finalizado

--- src/js/script.js
+++ src/js/script.js
@@ -1,45 +1,53 @@
 const buyButton = document.getElementById('buy-button')
+
 const cartForm = document.getElementById('cart-form')
 
 
-const addTicket = document.getElementById('add-ticket-one')
-const removeTicket = document.getElementById('remove-ticket-one')
-const ticket = document.getElementById('ticket-one')
-const ticketPrice = document.getElementById('ticket-price-one')
+
+const addTicket = document.getElementById('add-ticket')
+
+const removeTicket = document.getElementById('remove-ticket')
+
+const ticket = document.getElementById('ticket')
+
+const ticketPrice = document.getElementById('ticket-price')
+
 
 
 addTicket.addEventListener("click", (e) => {
     e.preventDefault();
     ticket.innerText = parseInt(ticket.innerText)+1
-    ticketPrice.innerText = "R$ " + parseInt(ticket.innerText)*90 + ",00 por " + ticket.innerText + " ingressos"
+    ticketPrice.innerText = "R$ " + parseInt(ticket.innerText)*90 + ",00 por " + ticket.innerText + " ingresso(s)"
 })
+
+
 
 removeTicket.addEventListener("click", (e) => {
     e.preventDefault();
-    if(parseInt(ticket.innerText) > 0) {
+
+    if(parseInt(ticket.innerText) !== 1) {
         ticket.innerText = parseInt(ticket.innerText)-1
-        ticketPrice.innerText = "R$ " + parseInt(ticket.innerText)*90 + ",00 por " + ticket.innerText + " ingressos"
-    } else {
-        ticketPrice.innerText = "R$ 90,00 por pessoa"
+        
+        ticketPrice.innerText = "R$ " + parseInt(ticket.innerText)*90 + ",00 por " + ticket.innerText + " ingresso(s)"
     }
 })
 
 
+
 cartForm.addEventListener("submit", (e) => {
     e.preventDefault();
+
     const nome = cartForm.nome.value;
+
     const email =  cartForm.email.value;
+
     const quantityTickets = ticket.innerText;
 
     window.localStorage.setItem('nome', nome)
+
     window.localStorage.setItem('email', email)
+
     window.localStorage.setItem('quantityTickets', quantityTickets)
+
     window.location.href = "finalizacao.html"
 })
-
-
-
-const addTicketTwo = document.getElementById('add-ticket-two')
-const removeTicketTwo = document.getElementById('remove-ticket-two')
-const ticketTwo = document.getElementById('ticket-two')
-const ticketPriceTwo = document.getElementById('ticket-price-two')
